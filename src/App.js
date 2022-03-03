584d4b1b1da486d0b95498175ecf46fcc910d213
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-03 12:39:42 +0300

    update App.js on importing specific componrnts and nested components

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import React from 'react';
 import 'devextreme/dist/css/dx.light.css';
 
 import Button from 'devextreme-react/button';
+import Chart, {ArgumentAxis, Series, Legend} from 'devextreme-react/chart';
+
+const data = [{
+	arg: 1990,
+	val: 5320816667
+}, {
+	arg: 2000,
+	val: 6127700428
+}, {
+	arg: 2010,
+	val: 6916183482
+}];
 
 class App extends React.Component {
-  render() {
-    return (
-        <Button
-            text="Click me"
-            onClick={this.sayHelloWorld}
-        />
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>
+					<Button
+						text="Click me"
+						onClick={this.sayHelloWorld}
+					/>
+				</div>
+				<div>
+					<Chart dataSource={data}>
+						<ArgumentAxis tickInterval={10}/>
+						<Series type="bar"/>
+						<Legend visible={false}/>
+					</Chart>
+				</div>
+			</div>
+		);
+	}
 
-  sayHelloWorld() {
-    alert('Hello world!')
-  }
+
+	sayHelloWorld() {
+		alert('Hello world!');
+	}
 }
 
 export default App;
