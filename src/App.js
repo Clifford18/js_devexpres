06b3bfa3c12bb3c3ec0b53a179786e431096e676
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-04 11:14:38 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -4,100 +4,134 @@ import 'devextreme/dist/css/dx.light.css';
 
 import Button from 'devextreme-react/button';
 import Chart, {ArgumentAxis, Series, Legend} from 'devextreme-react/chart';
-import {Scheduler, TextBox} from "devextreme-react";
-
-const data = [{
-	arg: 1990,
-	val: 5320816667
-}, {
-	arg: 2000,
-	val: 6127700428
-}, {
-	arg: 2010,
-	val: 6916183482
-}];
-
-const appointments = [{
-	text: 'Website Re-Design Plan',
-	startDate: new Date(2019, 4, 22, 9, 30),
-	endDate: new Date(2019, 4, 22, 11, 30)
-}, {
-	text: 'Book Flights to San Fran for Sales Trip',
-	startDate: new Date(2019, 4, 22, 12, 0),
-	endDate: new Date(2019, 4, 22, 13, 0),
-	allDay: true
-}, {
-	text: 'Install New Router in Dev Room',
-	startDate: new Date(2019, 4, 23, 10, 30),
-	endDate: new Date(2019, 4, 23, 16, 30)
-}];
+import {DataGrid, Scheduler, TextBox, Tooltip} from "devextreme-react";
+import {Column} from "devextreme-react/gantt";
 
 class App extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			text: 'TEXT'
-		};
-
-		this.handleValueChange = this.handleValueChange.bind(this);
-	}
+	columnEditorOptions = {width: 100};
 
 	render() {
 		return (
-			<div>
-				<div>
-					<Button
-						text="Click me"
-						onClick={this.sayHelloWorld}
-					/>
-				</div>
-				<div>
-					<Chart dataSource={data}>
-						<ArgumentAxis tickInterval={10}/>
-						<Series type="bar"/>
-						<Legend visible={false}/>
-					</Chart>
-				</div>
-				<div>
-					<TextBox
-						value={this.state.text}
-						onValueChange={this.handleValueChange}
-						valueChangeEvent="input"
-					/>
-					<br />
-					<div>{this.state.text}</div>
-				</div>
-				<div>
-					<Scheduler
-						dataSource={appointments}
-						height={600}
-						editing={false}
-						defaultCurrentDate={new Date(2019, 4, 22)}
-						startDayHour={9}
-					/>
-				</div>
-				<div>
-					<Button
-						disabled={false}
-						width={100}
-						text="Click me"
-					/>
-				</div>
-			</div>
+			<DataGrid>
+				<Column
+					editorOptions={this.columnEditorOptions}
+				/>
+			</DataGrid>
 		);
-	}
-
-
-	sayHelloWorld() {
-		alert('Hello world!');
-	}
-
-	handleValueChange(value) {
-		this.setState({
-			text: value.toUpperCase()
-		});
 	}
 }
 
-export default App;
+	export default App;
+
+//statis componenets
+
+// const data = [{
+// 	arg: 1990,
+// 	val: 5320816667
+// }, {
+// 	arg: 2000,
+// 	val: 6127700428
+// }, {
+// 	arg: 2010,
+// 	val: 6916183482
+// }];
+//
+// const appointments = [{
+// 	text: 'Website Re-Design Plan',
+// 	startDate: new Date(2019, 4, 22, 9, 30),
+// 	endDate: new Date(2019, 4, 22, 11, 30)
+// }, {
+// 	text: 'Book Flights to San Fran for Sales Trip',
+// 	startDate: new Date(2019, 4, 22, 12, 0),
+// 	endDate: new Date(2019, 4, 22, 13, 0),
+// 	allDay: true
+// }, {
+// 	text: 'Install New Router in Dev Room',
+// 	startDate: new Date(2019, 4, 23, 10, 30),
+// 	endDate: new Date(2019, 4, 23, 16, 30)
+// }];
+//
+// class App extends React.Component {
+//
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			text: 'TEXT'
+// 		};
+//
+// 		this.handleValueChange = this.handleValueChange.bind(this);
+// 	}
+//
+// 	columnEditorOptions = { width: 100 };
+//
+// 	render() {
+// 		return (
+// 			<div>
+// 				<div>
+// 					<Button
+// 						text="Click me"
+// 						onClick={this.sayHelloWorld}
+// 					/>
+// 				</div>
+// 				<div>
+// 					<Chart dataSource={data}>
+// 						<ArgumentAxis tickInterval={10}/>
+// 						<Series type="bar"/>
+// 						<Legend visible={false}/>
+// 					</Chart>
+// 				</div>
+// 				<div>
+// 					<TextBox
+// 						value={this.state.text}
+// 						onValueChange={this.handleValueChange}
+// 						valueChangeEvent="input"
+// 					/>
+// 					<br />
+// 					<div>{this.state.text}</div>
+// 				</div>
+// 				<div>
+// 					<Scheduler
+// 						dataSource={appointments}
+// 						height={600}
+// 						editing={false}
+// 						defaultCurrentDate={new Date(2019, 4, 22)}
+// 						startDayHour={9}
+// 					/>
+// 				</div>
+// 				<div>
+// 					<Button
+// 						disabled={false}
+// 						width={100}
+// 						text="Click me"
+// 					/>
+// 				</div>
+// 				<div>
+// 					<Chart>
+// 						<Tooltip
+// 							enabled={true}
+// 							format="thousands"
+// 						/>
+// 					</Chart>
+// 				</div>
+// 				<div>
+// 					<DataGrid>
+// 						<Column
+// 							editorOptions={this.columnEditorOptions}
+// 						/>
+// 					</DataGrid>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+//
+//
+// 	sayHelloWorld() {
+// 		alert('Hello world!');
+// 	}
+//
+// 	handleValueChange(value) {
+// 		this.setState({
+// 			text: value.toUpperCase()
+// 		});
+// 	}
+// }
